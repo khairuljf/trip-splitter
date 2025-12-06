6dc565d767e05f85446045aa5f0ae1a67e71cef2
Khairul <Khairuljf@gmail.com>
2025-12-06 08:10:58 +0600

    fix: env implemented remove app.json by app.config.js

--- app.config.js
+++ app.config.js
@@ -2,16 +2,67 @@ import 'dotenv/config';
 
 export default {
     expo: {
-        name: "YourApp",
-        slug: "your-app",
+        name: "trip-splitter",
+        slug: "trip-splitter",
+        version: "1.0.0",
+        orientation: "portrait",
 
+        icon: "./assets/images/icon.png",
+        scheme: "tripsplitter",
+        userInterfaceStyle: "automatic",
+        newArchEnabled: true,
+
+        ios: {
+            supportsTablet: true,
+        },
+
+        android: {
+            adaptiveIcon: {
+                backgroundColor: "#E6F4FE",
+                foregroundImage: "./assets/images/android-icon-foreground.png",
+                backgroundImage: "./assets/images/android-icon-background.png",
+                monochromeImage: "./assets/images/android-icon-monochrome.png",
+            },
+            edgeToEdgeEnabled: true,
+            predictiveBackGestureEnabled: false,
+        },
+
+        web: {
+            output: "static",
+            favicon: "./assets/images/favicon.png",
+            bundler: "metro",
+        },
+
+        plugins: [
+            "expo-router",
+            [
+                "expo-splash-screen",
+                {
+                    image: "./assets/images/splash-icon.png",
+                    imageWidth: 200,
+                    resizeMode: "contain",
+                    backgroundColor: "#ffffff",
+                    dark: {
+                        backgroundColor: "#000000",
+                    },
+                },
+            ],
+            "expo-sqlite",
+        ],
+
+        experiments: {
+            typedRoutes: true,
+            reactCompiler: true,
+        },
+
+        // ✅✅✅ FIREBASE ENV CONFIG
         extra: {
-            firebaseApiKey: process.env.FIREBASE_API_KEY,
-            firebaseAuthDomain: process.env.FIREBASE_AUTH_DOMAIN,
-            firebaseProjectId: process.env.FIREBASE_PROJECT_ID,
-            firebaseStorageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-            firebaseMessagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-            firebaseAppId: process.env.FIREBASE_APP_ID,
+            FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
+            FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
+            FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
+            FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
+            FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
+            FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
         },
     },
 };
